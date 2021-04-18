17c570730f74aab0f0a174c294772e5e922e590e
Nikonenko Vladislav <leve971@gmail.com>
2021-04-18 19:35:57 +0300

    commit 4week ACCORD v2.0

--- js/team.js
+++ js/team.js
@@ -1,17 +1,20 @@
 $(document).ready(() => {
-
   $('.team__item-name').on('click', function(e){
+    const accordActiveClose = $('.accord--active')
+    const sliderClose = $('team__item').hasClass(!accordActiveClose);
     if ($(this).hasClass('accord--active')){
       $(this).removeClass('accord--active');
       $(this).next('.team__item-content').slideUp();
-      $('.team__item-content').siblings('.team__item-content').slideUp();
-      console.log(e.target);
     }else{
+      let clonePos = $(this).prev('.team__item-head');
       $(this).addClass('accord--active');
       $(this).next('.team__item-content').slideDown(200);
       $(this).siblings('.team__item-content').slideDown(200);
-      $(this).siblings('.team__item').removeClass('accord--active');
-      console.log(e.target);
+      $(this).closest('.team__item').siblings('.team__item').find('.team__item-content').slideUp();
+      $(this).closest('.team__item').siblings('.team__item').find('.team__item-name').removeClass('accord--active');
+      $(clonePos).css({'display' : 'inline-block'})
+      let cloness = $(this).next('.team__item-content');
+      $(this).prev('.team__item-head').prependTo(cloness);
     };
   });
 });
